60bd1a2b4acf66dade64c3ddcba7306422cde7fe
Unknown <unge.tym@gmx.de>
2019-12-13 11:24:44 +0100

    Update to plugin for Blender 2.8

---
+++ Blender_CamGen/camgen_panel.py
@@ -0,0 +1,286 @@
+import bpy
+
+from bpy.props import (BoolProperty,IntProperty,FloatProperty,EnumProperty)
+from bpy.types import (Panel,Menu,Operator,PropertyGroup)
+
+from . import update
+
+
+# ------------------------------------------------------------------------
+#    Properties
+# ------------------------------------------------------------------------
+
+class CAMGEN_Properties(PropertyGroup):
+
+    prop_objective_list: EnumProperty(
+        name = "",
+        description = "Choose an objective.",
+        items = update.find_items
+        )
+
+    prop_objective_scale: FloatProperty(
+        name = "",
+        description = "Scaling of the objective - press 'Create Camera Model' after adjusting the scale in order to generate a new model.",
+        default = 1.0,
+        min = 0.001,
+        max = 10.0,
+        update = update.objective_scale
+        )
+
+    prop_vertex_count_radial: IntProperty(
+        name = "",
+        description="Latitudinal/radial number of vertices used for lens creation.",
+        default = 32,
+        min = 3,
+        max = 1000
+        )
+
+    prop_vertex_count_height: IntProperty(
+        name = "",
+        description="Longitudinal number of vertices used for lens creation.",
+        default = 36,
+        min = 3,
+        max = 1000
+        )
+
+    prop_sensor_width: FloatProperty(
+        name = "",
+        description = "Sensor Width in mm.",
+        default = 10.0,
+        min = 0.1,
+        max = 1000.0,
+        update = update.sensor_width
+        )
+    
+    prop_sensor_height: FloatProperty(
+        name = "",
+        description = "Sensor Height in mm.",
+        default = 10.0,
+        min = 0.1,
+        max = 1000.0,
+        update = update.sensor_height
+        )
+
+    prop_sensor_mainlens_distance: FloatProperty(
+        name = "",
+        description = "Objective-Sensor Distance in mm",
+        default = 100.0,
+        min = 0.1,
+        max = 10000.0,
+        update = update.sensor_mainlens_distance
+        )
+
+    prop_aperture_blades: IntProperty(
+        name = "",
+        description="Number of Aperture Blades.",
+        default = 6,
+        min = 3,
+        max = 100,
+        update = update.aperture_blades
+        )
+
+    prop_aperture_size: FloatProperty(
+        name = "",
+        description = "Aperture opening size in mm",
+        default = 100.0,
+        min = 0.1,
+        max = 10000.0,
+        update = update.aperture_size
+        )
+    
+    prop_aperture_angle: FloatProperty(
+        name = "",
+        description = "Aperture angle in degree",
+        default = 0.0,
+        min = 0.0,
+        max = 180.0,
+        update = update.aperture_angle
+        )
+
+    prop_wavelength: FloatProperty(
+        name = "",
+        description = "Wavelength in nm used for IOR calculation",
+        default = 587.6,
+        min = 380.0,
+        max = 780.0,
+        update = update.wavelength
+        )
+
+    prop_mla_enabled: BoolProperty(
+        name="",
+        description="Activate if microlens array should be used.",
+        default = True,
+        update = update.mla_enabled
+        )
+
+    prop_microlens_diam: FloatProperty(
+        name = "",
+        description="Diameter of a single microlens in mikrometers.",
+        default = 100.0,
+        min = 1,
+        max = 1000000.0,
+        update = update.microlens_diam
+        )
+
+    prop_mla_sensor_dist: FloatProperty(
+        name = "",
+        description = "Sets the distance between MLA and sensor/diffusor plane.",
+        default = 0.1,
+        min = 0.001,
+        max = 100.0,
+        update = update.mla_sensor_dist
+        )
+
+    prop_mla_type: EnumProperty(
+        name="",
+        description="Choose hexagonal or rectangular microlens layout.",
+        items=[ ('HEX', "Hexagonal Layout", ""),
+                ('RECT', "Rectangular Layout", ""),
+               ],
+        update = update.mla_type
+        )
+
+    prop_three_ml_types: BoolProperty(
+        name="",
+        description="Use three differently focused microlens types.",
+        default = True,
+        update = update.three_ml_types
+        )
+
+    prop_ml_type_1_f: FloatProperty(
+        name = "",
+        description = "Focal length of first microlens type in mm.",
+        default = 1.7,
+        min = 0.0,
+        max = 1000.0,
+        update = update.ml_type_1_f
+        )
+
+    prop_ml_type_2_f: FloatProperty(
+        name = "",
+        description = "Focal length of second microlens type in mm.",
+        default = 2,
+        min = 0.0,
+        max = 1000.0,
+        update = update.ml_type_2_f
+        )
+
+    prop_ml_type_3_f: FloatProperty(
+        name = "",
+        description = "Focal length of third microlens type in mm.",
+        default = 2.3,
+        min = 0.0,
+        max = 1000.0,
+        update = update.ml_type_3_f
+        )
+
+    prop_focal_distance: FloatProperty(
+        name = "",
+        description = "Object distance in cm the camera is focused on",
+        default = 50,
+        min = 1.0,
+        max = 10000.0,
+        update = update.focal_distance
+        )
+
+
+# ------------------------------------------------------------------------
+#    Main Panel
+# ------------------------------------------------------------------------
+
+class CAMGEN_PT_Main(bpy.types.Panel):
+    bl_idname = "CAMGEN_PT_Main"
+    bl_label = "Camera Generator"
+    bl_category = "Camera Generator Addon"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "scene"
+    bl_order = 999999  #Used by Blender for sorting the panels (large number means lower position)
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Objective")
+        row.prop(context.scene.camera_generator, "prop_objective_list")
+        row = layout.row()
+        row.label(text="Objective Scale")       
+        row.prop(context.scene.camera_generator, "prop_objective_scale")
+        row = layout.row()
+        row.label(text="Radial Vertices per Lens")       
+        row.prop(context.scene.camera_generator, "prop_vertex_count_radial")
+        row = layout.row()
+        row.label(text="Longitudinal Vertices per Lens")       
+        row.prop(context.scene.camera_generator, "prop_vertex_count_height")
+        row = layout.row()
+        row.operator('camgen.createcam', text="Create Camera Model")
+        row = layout.row()
+        row.operator('camgen.loadconfig', text="Load Camera Config")
+        row.operator('camgen.saveconfig', text="Save Camera Config")
+        row = layout.row()
+        row.label(text="")
+        row = layout.row()
+        row.label(text="Aperture Blades")
+        row.prop(context.scene.camera_generator, "prop_aperture_blades")
+        row = layout.row()
+        row.label(text="Aperture Scale")
+        row.prop(context.scene.camera_generator, "prop_aperture_size")
+        row = layout.row()
+        row.label(text="Aperture Angle")
+        row.prop(context.scene.camera_generator, "prop_aperture_angle")
+        row = layout.row()
+        row.label(text="Sensor Width in mm")
+        row.prop(context.scene.camera_generator, "prop_sensor_width")
+        row = layout.row()
+        row.label(text="Sensor Height in mm")
+        row.prop(context.scene.camera_generator, "prop_sensor_height")
+        row = layout.row()
+        row.label(text="Wavelength in nm")
+        row.prop(context.scene.camera_generator, "prop_wavelength")
+        row = layout.row()
+        row.label(text="Sensor-Objective Distance in mm")
+        row.prop(context.scene.camera_generator, "prop_sensor_mainlens_distance")
+        row = layout.row()
+        row.label(text="Focal distance in cm")
+        row.prop(context.scene.camera_generator, "prop_focal_distance")
+        row = layout.row()
+        row.label(text="")
+        row.operator('camgen.createcheckerboard', text="Create Checkerboard")
+
+
+# ------------------------------------------------------------------------
+#    MLA-Config Subpanel
+# ------------------------------------------------------------------------
+
+class CAMGEN_PT_MLAConfig(bpy.types.Panel):
+    bl_idname = "CAMGEN_PT_MLAConfig"
+    bl_label = "MLA Config"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_parent_id = "CAMGEN_PT_Main"
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Use MLA")
+        row.prop(context.scene.camera_generator, "prop_mla_enabled")
+        row = layout.row()
+        row.label(text="MLA Type")
+        row.prop(context.scene.camera_generator, "prop_mla_type") 
+        row = layout.row()
+        row.label(text="Microlens Diameter in um")
+        row.prop(context.scene.camera_generator, "prop_microlens_diam")
+        row = layout.row()
+        row.label(text="MLA-Sensor Distance in mm")
+        row.prop(context.scene.camera_generator, "prop_mla_sensor_dist")
+        row = layout.row()
+        row.label(text="Use three ML types")
+        row.prop(context.scene.camera_generator, "prop_three_ml_types")
+        row = layout.row()
+        row.label(text="Focal length ML type 1")
+        row.prop(context.scene.camera_generator, "prop_ml_type_1_f")
+        row = layout.row()
+        row.label(text="Focal length ML type 2")
+        row.prop(context.scene.camera_generator, "prop_ml_type_2_f")
+        row = layout.row()
+        row.label(text="Focal length ML type 3")
+        row.prop(context.scene.camera_generator, "prop_ml_type_3_f")
---
+++ Blender_CamGen/raytracer.py
@@ -0,0 +1,177 @@
+import math
+
+from . import data
+
+# dot product for 2d vectors
+def dot(v, w):
+    return v[0]*w[0]+v[1]*w[1]
+
+# calculates signed angle between two 2d vectors
+def angle(v, w):
+    signed_angle = math.atan2(w[1], w[0]) - math.atan2(v[1], v[0])
+    if signed_angle > math.pi:
+        signed_angle = signed_angle - 2.0 * math.pi
+    if signed_angle < -math.pi:
+        signed_angle = signed_angle + 2.0 * math.pi
+    return signed_angle
+
+# calculates the intersection of a ray and a lens surface
+def calculate_new_position(ray, lens):
+    # get lens surface data
+    center = lens['position']
+    radius = lens['radius']
+    flip = (radius < 0.0)
+    if flip:
+        radius = - radius
+    height = lens['semi_aperture']
+
+    # ray hitting a spherical surface
+    if radius > 0.0:
+        # calculate intersection of surface and ray
+        p_c = [ray[0] - center, ray[1]]
+        v = [math.cos(ray[2]), math.sin(ray[2])]
+
+        pc = dot(p_c,p_c)
+        pcv = dot(p_c,v)
+        squared = radius * radius - pc + pcv * pcv
+        if squared < 0.0:
+            return [0.0,0.0,180.0]
+
+        lambd = 0.0
+        if flip:
+            lambd = - pcv + math.sqrt(squared)
+        else:
+            lambd = - pcv - math.sqrt(squared)
+
+        intersection = [ray[0] + lambd * v[0], ray[1] + lambd * v[1]]
+        
+        if lambd < 0.0 or math.fabs(intersection[1]) > height:
+            return [0.0,0.0,180.0]
+        
+        return [intersection[0], intersection[1], ray[2]]
+
+    # ray hitting a flat surface
+    if radius == 0.0:
+        return [center, ray[1] + math.tan(ray[2]) * (center - ray[0]), ray[2]]
+
+# calculate new ray direction based on Snell's law
+def calculate_new_direction(ray, lens):
+    # get lens surface data
+    ior = lens['ior_ratio']
+    normal = [lens['position']-ray[0],-ray[1]]
+    if lens['radius'] < 0.0:
+        normal = [-normal[0], -normal[1]]
+    if lens['radius'] == 0.0:
+        normal = [1.0, 0.0]
+
+    direction = [math.cos(ray[2]), math.sin(ray[2])]
+    incident_angle = angle(normal, direction)
+    
+    # apply Snell's law
+    sin_of_angle = ior * math.sin(math.fabs(incident_angle))
+    if math.fabs(sin_of_angle) > 1.0: #reflection instead of transmission
+        return [0,0,180.0]
+
+    new_angle = math.copysign(math.asin(sin_of_angle), incident_angle)
+
+    normal_angle = angle([1.0, 0.0],normal)
+
+    #print("Normal "+str(normal[0])+" / "+str(normal[1])+" Direction "+str(direction[0])+" / "+str(direction[1]))
+    #print("Input angle "+str(ray[2]/math.pi*180.0)+" Output angle "+str((new_angle+normal_angle)/math.pi*180.0)+" Normal angle "+str(normal_angle/math.pi*180.0))
+    #print("Incident "+str(incident_angle/math.pi*180.0)+" IOR "+str(ior)+" Outgoing "+str(new_angle/math.pi*180.0))
+    #print("Position "+str(ray[0])+" / "+str(ray[1])+" Lens position "+str(lens['position']))
+
+    return [ray[0], ray[1], new_angle + normal_angle]
+
+# trace ray through one surface - returns a ray with angle 180.0 if tracing fails
+def trace_step(ray, lens):
+    new_pos_ray = calculate_new_position(ray, lens)
+    if new_pos_ray[2] == 180.0:
+        return new_pos_ray
+    else:
+        traced_ray = calculate_new_direction(new_pos_ray, lens)
+        return traced_ray
+
+# check if ray passes through aperture
+def check_aperture(ray):
+    # calculate intersection of ray and aperture plane
+    intersection = ray[1] - ray[0]*math.tan(ray[2])
+    # rays close to the aperture are ignored to take into account the non-circle aperture shape 
+    return (math.fabs(intersection) < 0.9*data.semi_aperture) 
+
+# trace a ray through all lens surfaces - returns -1.0 if tracing fails
+def trace_single_ray(ray):
+    #print("")
+    #print("New Ray!")
+    new_ray = ray
+
+    if data.aperture_index == -1:
+        if not check_aperture(ray):
+            return [0,0,180.0]
+
+    for i in range(0, len(data.objective)):
+        if i != data.aperture_index:
+            new_ray = trace_step(ray, data.objective[i])
+            ray = new_ray
+            if ray[2] == 180.0:
+                break
+        else: # check if ray passes through aperture
+            if not check_aperture(ray):
+                ray = [0,0,180.0]
+                break
+    
+    return ray
+
+# calculate the optimal sensor position for the given set of rays 
+def calculate_sensor_pos(rays):
+    if len(rays) == 0:
+        return -1
+
+    zeroes = []
+    for ray in rays:
+        b = ray[1]-ray[0]*math.tan(ray[2])
+        m = math.tan(ray[2])
+        zeroes.append(-b/m)
+    
+    circle_position = 0.0
+    for zero in zeroes:
+        circle_position = circle_position + zero
+
+    return circle_position/float(len(zeroes))
+
+# calculate the optimal sensor position for focusing on the desired distance - returns -1.0 if tracing fails
+def sensor_position_for_distance(distance):
+
+    # check if objective loaded
+    if len(data.objective) == 0:
+        print("No objective has been loaded.")
+        return -1
+
+    # set ray starting position
+    position = [-distance, 0.0]
+
+    # calculate maximum ray angle with respect to the x-axis
+    max_direction = [data.objective[0]['position'] - data.objective[0]['radius'] + distance, data.objective[0]['semi_aperture']]
+    max_angle = angle([1,0], max_direction)
+
+    # trace rays starting from desired distance
+    collected_rays = []
+    current_angle = max_angle
+    best_angle = 0
+    for i in range(1, 10):
+        traced_ray = trace_single_ray([position[0], position[1], current_angle])
+        if traced_ray[2] != 180.0:
+            best_angle = current_angle
+            current_angle = current_angle + (1.0/math.pow(2.0, i))*max_angle
+            if current_angle > max_angle:
+                break
+        else:
+            current_angle = current_angle - (1.0/math.pow(2.0, i))*max_angle
+
+    for i in range(1, 41):
+        resulting_ray = trace_single_ray([position[0], position[1], float(i)/40.0 * best_angle])
+        
+        if resulting_ray[2] != 180.0:
+            collected_rays.append(resulting_ray)
+
+    return calculate_sensor_pos(collected_rays)
